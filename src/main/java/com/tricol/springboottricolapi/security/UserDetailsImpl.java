e6598ac5b6c068c23e208d433e51749ee6608436
BadrEddine <badrdine03@gmail.com>
2025-12-20 20:00:06 +0100

    Fix UserDetailsImpl to respect user enabled status

--- src/main/java/com/tricol/springboottricolapi/security/UserDetailsImpl.java
+++ src/main/java/com/tricol/springboottricolapi/security/UserDetailsImpl.java
@@ -18,6 +18,7 @@ public class UserDetailsImpl implements UserDetails {
     private String username;
     private String email;
     private String password;
+    private boolean enabled;
     private Collection<? extends GrantedAuthority> authorities;
 
     public static UserDetailsImpl build(UserApp user, Collection<String> permissions) {
@@ -30,6 +31,7 @@ public class UserDetailsImpl implements UserDetails {
                 user.getUsername(),
                 user.getEmail(),
                 user.getPassword(),
+                user.getEnabled(),
                 authorities
         );
     }
@@ -51,6 +53,6 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return true;
+        return enabled;
     }
 }
